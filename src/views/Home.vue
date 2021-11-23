afaaf8c37ddbcf473acf2e06508d101e85fd414c
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-23 11:43:04 -0600

    connect indexgifs to console log.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>GIFS</h1>
+
   </div>
 </template>
 
@@ -10,10 +11,16 @@
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
+        
       };
     },
-    created: function () {},
-    methods: {},
+    created: function () {
+      this.indexGifs();
+    },
+    methods: {
+      indexGifs: function () {
+        console.log('hello')
+      }
+    },
   };
 </script>
